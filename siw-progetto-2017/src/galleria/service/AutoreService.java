caa3a1f97be596956b233433b09f80c4ed7dd009
petruz93 <matteop93@hotmail.it>
2017-06-03 18:32:17 +0200

    [PERSISTENCE] creato repository per gli autori; create classi per la persistenza e operazioni CRUD; completata classe AutoreService.java

--- siw-progetto-2017/src/galleria/service/AutoreService.java
+++ siw-progetto-2017/src/galleria/service/AutoreService.java
@@ -1,33 +1,65 @@
 package galleria.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 import galleria.model.Autore;
+import galleria.persistence.jpa.AutoreRepository;
 
 public class AutoreService {
+	private EntityManager em;
+	private AutoreRepository repository;
+	
+	public AutoreService() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("galleria-unit");
+		this.em = emf.createEntityManager();
+		this.repository = new AutoreRepository(this.em);
+	}
 	
 	public Autore insertAutore(Autore autore) {
-		// TODO
-		return autore;
+		EntityTransaction tx = this.em.getTransaction();
+		tx.begin();
+		Autore autoreEntity = this.repository.save(autore);
+		tx.commit();
+		
+		return autoreEntity;
 	}
 	
 	public List<Autore> getAutori() {
-		// TODO
-		return new ArrayList<>();
+		EntityTransaction tx = this.em.getTransaction();
+		tx.begin();
+		List<Autore> autori = this.repository.findAll();
+		tx.commit();
+		
+		return autori;
 	}
 	
 	public Autore getOneAutore(Long id) {
-		// TODO
-		return new Autore();
+		EntityTransaction tx = this.em.getTransaction();
+		tx.begin();
+		Autore autore = this.repository.findOne(id);
+		tx.commit();
+		
+		return autore;
 	}
 	
-	public void deleteAutore(Autore autore) {
-		// TODO
+	public void removeAutore(Autore autore) {
+		EntityTransaction tx = this.em.getTransaction();
+		tx.begin();
+		this.repository.delete(autore);
+		tx.commit();
 	}
 	
-	public void deleteAutoreByID(Long id) {
-		// TODO
+	public void removeAutoreByID(Long id) {
+		EntityTransaction tx = this.em.getTransaction();
+		tx.begin();
+		Autore autore = this.repository.findOne(id);
+		this.repository.delete(autore);
+		tx.commit();
 	}
 
 }
